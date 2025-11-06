f2949b39b8eb9359f4e35a834d15fcd91fc3b2ca
mdsafeeuddin <mdsafeeuddin@gmail.com>
2025-11-06 11:06:26 +0530

    added blog module.

---
+++ src/app/blog/blog-details/blog-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-details',
+  standalone: false,
+  templateUrl: './blog-details.html',
+  styleUrl: './blog-details.css',
+})
+export class BlogDetails {
+
+}
---
+++ src/app/blog/blog-routing-module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BlogList } from './blog-list/blog-list';
+import { BlogDetails } from './blog-details/blog-details';
+
+const routes: Routes = [
+  {path: '', component: BlogList},
+  {path: ':id', component: BlogDetails}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BlogRoutingModule { }
