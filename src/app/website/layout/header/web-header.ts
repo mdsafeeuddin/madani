cf38c1664e45ebc52a32a070fbd362a9c86ad6a7
mdsafeeuddin <mdsafeeuddin@gmail.com>
2026-02-07 23:17:16 +0530

    moved scroll to top to werbsite component.

--- src/app/website/layout/header/web-header.ts
+++ src/app/website/layout/header/web-header.ts
@@ -8,18 +8,8 @@ import { Component } from '@angular/core';
 })
 export class WebHeader {
   constructor(){
-    window.addEventListener('scroll', () => {
-      const btn = document.getElementById('scrollTopBtn');
-      if(!btn) return;
-      if (window.pageYOffset > 300){
-        btn.classList.remove('d-none');
-      }else{
-        btn.classList.add('d-none');
-      }
-    })
+    
   }
 
-  scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-}
+  
 }
--- src/app/website/website.ts
+++ src/app/website/website.ts
@@ -8,5 +8,19 @@ import { Component } from "@angular/core";
 })
 
 export class Website{
+constructor(){
+  window.addEventListener('scroll', () => {
+      const btn = document.getElementById('scrollTopBtn');
+      if(!btn) return;
+      if (window.pageYOffset > 300){
+        btn.classList.remove('d-none');
+      }else{
+        btn.classList.add('d-none');
+      }
+    })
+}
 
+scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 }
