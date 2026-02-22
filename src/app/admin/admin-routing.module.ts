8aecfcb80c5729ea240f19adf0408fa747133b92
mdsafeeuddin <mdsafeeuddin@gmail.com>
2026-02-22 16:35:37 +0530

    updated admin routing to load the admin home by default. and fixed header css conflict for website and admin.

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -13,6 +13,11 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children:[
       {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
         path: 'home',
         component: AdminHome
       },
